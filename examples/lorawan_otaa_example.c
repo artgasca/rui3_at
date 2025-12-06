ca17abb8a06e21131864d13dd33a62f02fec59cb
Arturo Gasca <arturogasca.90@gmail.com>
2025-12-06 14:07:24 -0600

    tx uplink ok

--- examples/lorawan_otaa_example.c
+++ examples/lorawan_otaa_example.c
@@ -22,8 +22,30 @@
 #define RUI3_AT_DEBUG    1
 #include "../rui3_at.h"
 #include "../rui3_at.c"
+#include "v2.h"
 
 
+/* Protolink Default functions
+ * Active define for use in main
+ * Interrupts use for counters INT0, INT1
+ * SMT1 and SMT use for counters
+ * Timer0 config for 1 second, consume in protolink_one_second()
+ */
+// Numero de interrupciones de TIMER0 para ~1 segundo
+// 0.013s * 77 ? 1001 ms
+int16 NInts = 77;
+
+// VARIABLES GLOBALES
+int16 C_Ints = 0;   // Contador de interrupciones ocurridas
+int1 Flag = 0;     // Flag que cambia cada ~1 segundo
+
+// Ejemplo de uso: hacer algo cada vez que cambie Flag (~1s)
+int1 lastFlag = 0;
+
+
+
+#define UPLINK_TIME 20
+int16 uplink_time_count = 0;
 
 // ISR de RX UART
 #INT_RDA2
@@ -33,13 +55,34 @@ void RDA2_isr(void)
    rui3_at_uart_rx_isr(c);
 }
 
-void main(void) {
 
+#INT_TIMER0
+void TIMER0_isr(void)
+{
+    C_Ints++;
+    if(C_Ints >= NInts){
+        C_Ints = 0;
+        Flag = !Flag; //Toggle cada 1s
+    }
+    clear_interrupt(INT_TIMER0);
+}
+
+int1 protolink_one_second(void){
+    if(Flag != lastFlag){
+        lastFlag = flag;
+        return true;
+    }
+    return false;
+}
+void main(void) {
+    delay_ms(100);
     protolink_io_init();
     output_low(LED1);
     output_low(LED2);
     rui3_event_t evt;
     rui3_status_t st;
+    setup_timer_0(T0_INTERNAL | T0_DIV_256 | T0_8_BIT);
+    enable_interrupts(INT_TIMER0);
     enable_interrupts(INT_RDA2);
     enable_interrupts(GLOBAL);
 
@@ -101,16 +144,31 @@ void main(void) {
           {
              case RUI3_EVT_RX:
                 // procesar downlink
+                 protolink_debug_msg("Process downlink\r\n");
                 break;
 
              case RUI3_EVT_TX_DONE:
              case RUI3_EVT_CONF_OK:
                 // TX OK
+                 protolink_debug_msg("TX Done!\r\n");
                 break;
 
              default:
                 break;
           }
+       }
+       if(protolink_one_second()){
+           uplink_time_count++;
+           if (uplink_time_count >= UPLINK_TIME) {
+               uplink_time_count = 0;
+               protolink_debug_msg("TIME TO SEND!\r\n");
+               st = rui3_at_send_uplink_start(1,(char*)"AABBCC");
+               if(st != RUI3_ST_OK){
+                   protolink_debug_msg("Algo fallo en el envio\r\n");
+               }
+
+            }
+
        }
 
        // Aquí puedes hacer tu FSM para mandar uplinks:
