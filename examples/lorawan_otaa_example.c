ff16a8d5831587efb3251edad82f6f78092b6fd7
Arturo Gasca <arturogasca.90@gmail.com>
2025-12-20 11:04:54 -0600

    update v2 header

--- examples/lorawan_otaa_example.c
+++ examples/lorawan_otaa_example.c
@@ -4,8 +4,8 @@
  *
  * Created on 06 de Diciembre de 2025, 10:32 AM
  */
-
-#include "v1.h"
+#define PROTOLINK_DEFAULT true
+#include "v2.h"
 
 
 
@@ -24,66 +24,20 @@
 #include "v2.h"
 
 
-/* Protolink Default functions
- * Active define for use in main
- * Interrupts use for counters INT0, INT1
- * SMT1 and SMT use for counters
- * Timer0 config for 1 second, consume in protolink_one_second()
- */
-// Numero de interrupciones de TIMER0 para ~1 segundo
-// 0.013s * 77 ? 1001 ms
-int16 NInts = 77;
-
-// VARIABLES GLOBALES
-int16 C_Ints = 0;   // Contador de interrupciones ocurridas
-int1 Flag = 0;     // Flag que cambia cada ~1 segundo
-
-// Ejemplo de uso: hacer algo cada vez que cambie Flag (~1s)
-int1 lastFlag = 0;
-
-
 
 #define UPLINK_TIME 20
 int16 uplink_time_count = 0;
-//
-//// ISR de RX UART
-//#INT_RDA2
-//void RDA2_isr(void)
-//{
-//   unsigned int8 c = fgetc(RUI3_UART);
-//   rui3_at_uart_rx_isr(c);
-//}
 
 
-#INT_TIMER0
-void TIMER0_isr(void)
-{
-    C_Ints++;
-    if(C_Ints >= NInts){
-        C_Ints = 0;
-        Flag = !Flag; //Toggle cada 1s
-    }
-    clear_interrupt(INT_TIMER0);
-}
-
-int1 protolink_one_second(void){
-    if(Flag != lastFlag){
-        lastFlag = flag;
-        return true;
-    }
-    return false;
-}
 void main(void) {
     delay_ms(100);
     protolink_io_init();
+    protolink_timer0_init();
     output_low(LED1);
     output_low(LED2);
     rui3_event_t evt;
     rui3_status_t st;
-    setup_timer_0(T0_INTERNAL | T0_DIV_256 | T0_8_BIT);
-    enable_interrupts(INT_TIMER0);
-    enable_interrupts(INT_RDA2);
-    enable_interrupts(GLOBAL);
+    
 
     rui3_at_init();
 
